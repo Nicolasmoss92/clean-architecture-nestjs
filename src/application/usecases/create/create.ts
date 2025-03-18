dd98d6bd714be137ca503b59f3d7752097026573
00043963 <00043963@randon.com.br>
2025-03-17 21:06:16 -0300

    feat: controller

---
+++ src/application/usecases/create/create.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PetService {
+    constructor('') private readonly
+    getHello(): string {
+        return 'Hello Pet!';
+    }
+}
