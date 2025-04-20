cf5635bad8ac5f34857cb4456d274ab148722639
00043963 <00043963@randon.com.br>
2025-04-20 20:14:44 -0300

    feat: reafctoring

--- src/core/useCases/createPet/create-pet.usecase.ts
+++ src/core/useCases/createPet/create-pet.usecase.ts
@@ -1,9 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ICreatePetUseCase } from './create.interface';
+import { ICreatePetUseCase } from './create-pet.interface';
 import { IPetRepository } from 'src/core/ports/pet.repository';
-import { CreatePetDto } from 'src/presentation/controller/create/create.dto';
+import { CreatePetDto } from 'src/presentation/controller/createPet/create-pet..dto';
 import { Pet } from 'src/core/entities/pet';
-import { PetAlreadyExistsException } from 'src/core/exceptions/pet-conflict';
+import { PetAlreadyExistsException } from 'src/core/exceptions/pet-already-exists.exception';
 
 @Injectable()
 export class CreatePetUsecase implements ICreatePetUseCase {
