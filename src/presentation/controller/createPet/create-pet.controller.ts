801e8613de833cbe131789bd71b794658a9e98eb
00043963 <00043963@randon.com.br>
2025-04-21 12:51:17 -0300

    feat: test

--- src/presentation/controller/createPet/create-pet.controller.ts
+++ src/presentation/controller/createPet/create-pet.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
 import { ICreatePetUseCase } from 'src/core/useCases/createPet/create-pet.interface';
-import { CreatePetDto } from './create-pet..dto';
+import { CreatePetDto } from './create-pet.dto';
 import { Pet } from 'src/core/entities/pet';
 import { PetMapper } from './pet.mapper';
 
