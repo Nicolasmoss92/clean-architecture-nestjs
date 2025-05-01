3c3bf5fd5999562124b63dde99ad5f7a955da04e
00043963 <00043963@randon.com.br>
2025-05-01 19:42:37 -0300

    feat: validate id

--- src/core/useCases/createPet/create-pet.usecase.ts
+++ src/core/useCases/createPet/create-pet.usecase.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ICreatePetUseCase } from './create-pet.interface';
 import { IPetRepository } from 'src/core/ports/pet.repository';
 import { Pet } from 'src/core/entities/pet';
+import { PetAlreadyExistsException } from 'src/core/exceptions/pet-already-exists.exception';
 
 @Injectable()
 export class CreatePetUsecase implements ICreatePetUseCase {
@@ -10,6 +11,12 @@ export class CreatePetUsecase implements ICreatePetUseCase {
   ) { }
 
   async create(body: Pet): Promise<any> {
+    const pet = await this.petRepository.getPetById(body.id);
+
+    if (pet) {
+      throw new PetAlreadyExistsException;
+    }
+
     await this.petRepository.create(body);
   }
 }
