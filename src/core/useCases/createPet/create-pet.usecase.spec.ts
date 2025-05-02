1c3c95458aa249ddb0f8a5fab7c7515187339a57
00043963 <00043963@randon.com.br>
2025-05-02 08:16:48 -0300

    feat:unite test

--- src/core/useCases/createPet/create-pet.usecase.spec.ts
+++ src/core/useCases/createPet/create-pet.usecase.spec.ts
@@ -1,10 +1,12 @@
 import { CreatePetUsecase } from './create-pet.usecase';
 import { Pet } from '../../../core/entities/pet';
+import { PetAlreadyExistsException } from '../../../core/exceptions/pet-already-exists.exception';
 
 describe('CreatePetUsecase', () => {
   let useCase: CreatePetUsecase;
   const mockPetRepository = {
     create: jest.fn(),
+    getPetById: jest.fn(),
   };
 
   beforeEach(() => {
@@ -26,9 +28,27 @@ describe('CreatePetUsecase', () => {
       updated_at: new Date(),
     });
 
+    mockPetRepository.getPetById.mockResolvedValue(null);
+
     await useCase.create(pet);
 
-    expect(mockPetRepository.create).toHaveBeenCalledTimes(1);
     expect(mockPetRepository.create).toHaveBeenCalledWith(pet);
+  });
+
+  it('deve lançar PetAlreadyExistsException se o pet já existir', async () => {
+    const pet: Pet = new Pet({
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      name: 'Rex',
+      species: 'dog',
+      age: 5,
+      ownerId: '321e4567-e89b-12d3-a456-426614174000',
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    mockPetRepository.getPetById.mockResolvedValue(pet);
+
+    await expect(useCase.create(pet)).rejects.toThrow(PetAlreadyExistsException);
+    expect(mockPetRepository.create).not.toHaveBeenCalled();
   });
 });
