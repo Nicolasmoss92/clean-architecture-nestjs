1488bd8df71f988ddd4ee2fa54c4e4f6aedc0365
00043963 <00043963@randon.com.br>
2025-05-06 07:49:17 -0300

    feat: param

--- src/presentation/controller/http/getPetById/get-by-id.ts
+++ src/presentation/controller/http/getPetById/get-by-id.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Inject } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Inject, Param } from "@nestjs/common";
 import { GetPetByIdDto } from "./get-pet-by-id.dto";
 import { GetPetByIdResponse } from "./get-pet-by-id-response.dto";
 import { GetPetByIdUseCase } from "src/core/useCases/getPetById/get-pet-by-id.usecase";
@@ -11,9 +11,10 @@ export class GetPetByIdController {
   
     @Get()
     @HttpCode(200)
-    async handle(@Body() getPetById: GetPetByIdDto): Promise<GetPetByIdResponse> {
+    async handle(@Param() getPetById: GetPetByIdDto): Promise<GetPetByIdResponse> {
       const { id } = getPetById;
       const pet = await this.getPetByIdUseCase.getById(id);
+      
       return pet;
     }
 }
