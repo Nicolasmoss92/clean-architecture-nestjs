5c20da33107d97f1d34f81e6f17ec9daac7a5dfc
Nicolas <nicolas.moss@dbserver.com.br>
2025-07-30 16:08:01 -0300

    feat: commit test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { KnexModule } from './infrastructure/database/knex.module';
 import { CreatePetUsecase } from './core/useCases/createPet/create-pet.usecase';
 import { RepositoriesModule } from './infrastructure/repositories/module/repositories.module';
-import { CreatePetController } from './presentation/controller/http/createPet/create-pet';
+import { CreatePetController } from './presentation/controller/http/createOrder/create-pet';
 import { ListPetsController } from './presentation/controller/http/listPets/list-pets';
 import { ListPetsUseCase } from './core/useCases/listPets/list-pets.usecase';
 import { DeletePetController } from './presentation/controller/http/deletePetById/delete-pet-by-id';
